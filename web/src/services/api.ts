4bc723b5aa0e4dc43790f2c200849d763a1694df
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2020-11-12 19:23:06 -0300

    Update api.

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,18 +1,23 @@
 import axios from 'axios';
 
-import { getToken, isAuthenticated } from '../config/auth';
+// import { getToken, isAuthenticated } from '../config/auth';
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:3333',
+// });
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://entregas-cariri-backend.vercel.app/',
 });
 
-if (!isAuthenticated) {
-  api.interceptors.request.use(async config => {
-    const token = getToken();
-    if (token) config.headers.Authorization = `Bearer ${token}`;
+
+// if (!isAuthenticated) {
+//   api.interceptors.request.use(async config => {
+//     const token = getToken();
+//     if (token) config.headers.Authorization = `Bearer ${token}`;
     
-    return config;
-  });
-} 
+//     return config;
+//   });
+// } 
 
 export default api;
